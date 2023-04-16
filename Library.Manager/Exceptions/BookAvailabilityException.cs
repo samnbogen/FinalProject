1946eeff5bf0a8643ff588c99e1e808a6d29dc6f
lisakamdem <120063552+lisakamdem@users.noreply.github.com>
2023-04-15 21:44:21 -0600

    Add files via upload

---
+++ Library.Manager/Exceptions/BookAvailabilityException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Library.Manager.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//99
+
+namespace Library.Manager.Exceptions
+{
+    //This is exception is thrown when the book is not available
+    public class BookAvailabilityException : Exception
+    {
+        public BookAvailabilityException() :  base("Sorry, Looks like this book is unavilable at the moment") { }
+    }
+}
---
+++ Library.Manager/Exceptions/BookNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Manager.Exceptions
+{
+    //This exception is throws when the book is not found 
+    public class BookNotFoundException : Exception
+    {
+        public BookNotFoundException() : base("Sorry, we cannot find the book you're looking for") { }
+    }
+}
