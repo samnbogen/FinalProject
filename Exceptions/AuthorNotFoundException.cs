a0ab2d157770d8ddac6521a82abced674c03f442
lisakamdem <120063552+lisakamdem@users.noreply.github.com>
2023-04-17 12:21:57 -0600

    April 17, push

---
+++ Exceptions/AuthorNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Exceptions
+{
+    //This exception is throws when it cannot find the author
+    public class AuthorNotFoundException : Exception
+    {
+        public AuthorNotFoundException() : base("Sorry we canno find the author you're looking for") { }
+    }
+}
